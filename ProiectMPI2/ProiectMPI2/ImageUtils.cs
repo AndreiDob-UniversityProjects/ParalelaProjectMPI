d69855282a3309e710299d447b200af4faa996fd
AndreiDob <dobrea.andrei1998@gmail.com>
2020-01-11 18:57:02 +0200

    mpi done

---
+++ ProiectMPI2/ProiectMPI2/ImageUtils.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPI_ImageFilter
+{
+    class ImageUtils
+    {
+        private int height;
+        private int width;
+
+        private int[,] r;
+        private int[,] g;
+        private int[,] b;
+        private int[,,] resultMatrix;
+
+        public ImageUtils(Bitmap image)
+        {
+            height = image.Height;
+            width = image.Width;
+            
+            r = new int[height, width];
+            g = new int[height, width];
+            b = new int[height, width];
+            resultMatrix = new int[height, width, 3];
+
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    Color pixelColor = image.GetPixel(j, i);
+                    r[i, j] = pixelColor.R;
+                    g[i, j] = pixelColor.G;
+                    b[i, j] = pixelColor.B;
+                }
+        }
+
+        public int[, ,] sobbelOperatorForSection(Bitmap image, int begin, int end, int blurringDegree)
+        {
+            for (int i = begin; i < end; i++)
+            {
+                for (int j = 1; j < width-1; j++)
+                {
+                    int[] averagePixelArray = convolutePixel(i, j, blurringDegree);
+                    for (int k = 0; k < averagePixelArray.Length; k++)
+                    {
+                        resultMatrix[i, j, k] = averagePixelArray[k];
+                    }
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        private int[] convolutePixel(int x, int y, int size)
+        {
+            int[] resultPixel = new int[3];
+            resultPixel[0] = 0;
+            resultPixel[1] = 0;
+            resultPixel[2] = 0;
+
+            int[,] kernel = new int[3, 3];
+            for (int i = x - 1; i <= x + 1; i++)
+               for (int j = y - 1; j <= y + 1; j++)
+                    {
+                       kernel[i - x + 1, j - y + 1] = (r[i, j]+g[i,j]+b[i,j])/3;
+                    }
+                            
+            int resultX = 0;
+            resultX += -1 * (kernel[0, 0] + kernel[2, 0]);
+            resultX += -2 * kernel[1, 0];
+            resultX += 1 * (kernel[0, 2] + kernel[2, 2]);
+            resultX += 2 * kernel[1, 2];
+
+            int resultY = 0;
+            resultY += -1 * (kernel[0, 0] + kernel[0, 2]);
+            resultY += -2 * kernel[0, 1];
+            resultY += 1 * (kernel[2, 0] + kernel[2, 2]);
+            resultY += 2 * kernel[2, 1];
+
+            int resultFinal = Convert.ToInt32(Math.Sqrt(Math.Pow(resultX, 2) + Math.Pow(resultY, 2)));
+
+            if (resultFinal > 255)
+            {
+                resultFinal = 255;
+            }
+
+            resultPixel[0] = resultFinal;
+            resultPixel[1] = resultFinal;
+            resultPixel[2] = resultFinal;
+
+            return resultPixel;
+        }
+
+        public void saveModifiedImage(int[,,] pixelMatrix, string oldPath, string newPath)
+        {
+            Bitmap newImage = new Bitmap(oldPath);
+            for (int i = 1; i < height-1; i++)
+            {
+                for (int j = 1; j < width-1; j++)
+                {
+                    int[] pixelColorArray = new int[3];
+                    pixelColorArray[0] = pixelMatrix[i, j, 0];
+                    pixelColorArray[1] = pixelMatrix[i, j, 1];
+                    pixelColorArray[2] = pixelMatrix[i, j, 2];
+
+                    Color pixelColor = Color.FromArgb(pixelColorArray[0], pixelColorArray[1], pixelColorArray[2]);
+                    newImage.SetPixel(j, i, pixelColor);
+                }
+            }
+
+            newImage.Save(newPath, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        public int[,,] createPixelMatrix(int[,] r, int[,] g, int[,] b)
+        {
+            int[,,] pixelMatrix = new int[height, width, 3];
+
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    int[] pixel = new int[3];
+                    pixel[0] = r[i, j];
+                    pixel[1] = g[i, j];
+                    pixel[2] = b[i, j];
+                    for (int k = 0; k < pixel.Length; k++)
+                    {
+                        pixelMatrix[i, j, k] = pixel[k];
+                    }
+                }
+
+            return pixelMatrix;
+        }
+    }
+}
---
+++ ProiectMPI2/ProiectMPI2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using MPI;
+
+namespace MPI_ImageFilter
+{
+    class Program
+    {
+        public static void master(string oldPath, string newPath)
+        {
+            DateTime start = DateTime.Now;
+            Console.WriteLine("Loading image... ");
+            Bitmap image = new Bitmap(oldPath);
+            Console.WriteLine("Image loaded ");
+
+            int n = Communicator.world.Size;
+            int begin = 1;
+            int end = 1;
+            int length = image.Height / (n - 1);
+
+            int height = image.Height;
+            int width = image.Width;
+            int blurringDegree = 3;
+
+
+            //sending data to slaves
+            Console.WriteLine("Sending data... ");
+            for (int i = 1; i < n; i++)
+            {
+                begin = end;
+                end = end + length;
+                if (i == n - 1)
+                {
+                    end = image.Height - 1;
+                }
+
+                Communicator.world.Send(image, i, 0);
+                Communicator.world.Send(begin, i, 0);
+                Communicator.world.Send(end, i, 0);
+                Communicator.world.Send(blurringDegree, i, 0);
+            }
+            Console.WriteLine("Data sent ");
+
+            //receiving data from slaves
+            Console.WriteLine("Waiting for responses... ");
+            int[,,] results = new int[height, width, 3];
+            for (int i = 1; i < n; i++)
+            {
+                int beginIndex = Communicator.world.Receive<int>(i, 0);
+                int endIndex = Communicator.world.Receive<int>(i, 0);
+                int[,,] partialResult = Communicator.world.Receive<int[,,]>(i, 0);
+
+                Console.WriteLine("\t Slave "+ i +" finished");
+
+                for (int heightIndex = beginIndex; heightIndex < endIndex; heightIndex++)
+                {
+                    for (int widthIndex = 0; widthIndex < width; widthIndex++)
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            results[heightIndex, widthIndex, k] = partialResult[heightIndex, widthIndex, k];
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Responses obtained ");
+
+            //creating new image and saving it
+            Console.WriteLine("Saving new image... ");
+            ImageUtils imageOperations = new ImageUtils(image);
+            imageOperations.saveModifiedImage(results, oldPath, newPath);
+            Console.WriteLine("Saved image " + newPath + " " + (DateTime.Now - start));
+        }
+
+        public static void slave()
+        {
+            Bitmap image = Communicator.world.Receive<Bitmap>(0, 0);
+            int begin = Communicator.world.Receive<int>(0, 0);
+            int end = Communicator.world.Receive<int>(0, 0);
+            int blurringDegree = Communicator.world.Receive<int>(0, 0);
+
+            ImageUtils imageOperations = new ImageUtils(image);
+            int[,,] result = imageOperations.sobbelOperatorForSection(image, begin, end, blurringDegree);
+
+            Communicator.world.Send(begin, 0, 0);
+            Communicator.world.Send(end, 0, 0);
+            Communicator.world.Send(result, 0, 0);
+        }
+    
+
+    static void Main(string[] args)
+        {
+
+            using (new MPI.Environment(ref args))
+            {
+                if (Communicator.world.Rank == 0)
+                {
+                    //master process
+                    master("C:\\Users\\Asus\\source\\repos\\ProiectMPI2\\ProiectMPI2\\image.jpg", "C:\\Users\\Asus\\source\\repos\\ProiectMPI2\\ProiectMPI2\\image_sobbel.jpg");
+                }
+                else
+                {
+                    //child process
+                    slave();
+                }
+            }
+        }
+    }
+}
